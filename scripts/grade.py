9657aff461ba8d69bbcf6621c350b23e1321c6a7
parautenbach <parautenbach@gmail.com>
2019-08-01 14:30:05 +0200

    Fixing pylint issues

--- scripts/grade.py
+++ scripts/grade.py
@@ -5,15 +5,12 @@
 
 import os.path
 import warnings
-
 from argparse import ArgumentParser
 
 import gpxpy
 import gpxpy.gpx
 import matplotlib.pylab as plt
-
 from matplotlib import collections, colors
-
 import numpy as np
 import scipy.signal as signal
 import seaborn as sns
@@ -114,15 +111,15 @@ def calculate_metrics(markers, distances, cumulative_distances, times, elevation
     speed_averages = []
     cadence_averages = []
     cadence_percentages = []
-    for (idx, marker) in enumerate(markers[1:]):
+    for (idx, _) in enumerate(markers[1:]):
         start = markers[idx]
         end = markers[idx + 1]
-        d = sum(distances[start:end])
-        e = elevations[end] - elevations[start]
-        if d == 0:
+        delta_distance = sum(distances[start:end])
+        delta_elevation = elevations[end] - elevations[start]
+        if delta_distance == 0:
             grade = 0
         else:
-            grade = e/d*100
+            grade = delta_elevation/delta_distance*100
         # off by one error
         grades.extend(np.ones(end - start)*grade)
         heart_rate_averages.extend(np.ones(end - start)*np.average(heart_rates[start:end]))
@@ -151,51 +148,51 @@ def get_figure(args, heart_rates, cadences):
     # 1, 1, 1
     if args.plot_speed and (args.plot_heart_rate and heart_rates) and (args.plot_cadence and cadences):
         rows = 4
-        (f, axes) = plt.subplots(rows, 1, sharex=True)
+        (fig, axes) = plt.subplots(rows, 1, sharex=True)
         (ax_elevation, ax_speed, ax_hr, ax_cadence) = axes
     # speed, hr, not cad
     # 1, 1, 0
     elif args.plot_speed and (args.plot_heart_rate and heart_rates) and not (args.plot_cadence and cadences):
         rows = 3
-        (f, axes) = plt.subplots(rows, 1, sharex=True)
+        (fig, axes) = plt.subplots(rows, 1, sharex=True)
         (ax_elevation, ax_speed, ax_hr) = axes
     # speed, not hr, cad
     # 1, 0, 1
     elif args.plot_speed and not (args.plot_heart_rate and heart_rates) and (args.plot_cadence and cadences):
         rows = 3
-        (f, axes) = plt.subplots(rows, 1, sharex=True)
+        (fig, axes) = plt.subplots(rows, 1, sharex=True)
         (ax_elevation, ax_speed, ax_cadence) = axes
     # speed, not hr, not cad
     # 1, 0, 0
     elif args.plot_speed and not (args.plot_heart_rate and heart_rates) and not (args.plot_cadence and cadences):
         rows = 2
-        (f, axes) = plt.subplots(rows, 1, sharex=True)
+        (fig, axes) = plt.subplots(rows, 1, sharex=True)
         (ax_elevation, ax_speed) = axes
     # not speed, hr, cad
     # 0, 1, 1
     elif not args.plot_speed and (args.plot_heart_rate and heart_rates) and (args.plot_cadence and cadences):
         rows = 3
-        (f, axes) = plt.subplots(rows, 1, sharex=True)
+        (fig, axes) = plt.subplots(rows, 1, sharex=True)
         (ax_elevation, ax_hr, ax_cadence) = axes
     # not speed, hr, not cad
     # 0, 1, 0
     elif not args.plot_speed and (args.plot_heart_rate and heart_rates) and not (args.plot_cadence and cadences):
         rows = 2
-        (f, axes) = plt.subplots(rows, 1, sharex=True)
+        (fig, axes) = plt.subplots(rows, 1, sharex=True)
         (ax_elevation, ax_hr) = axes
     # not speed, not hr, cad
     # 0, 0, 1
     elif not args.plot_speed and not (args.plot_heart_rate and heart_rates) and args.plot_cadence:
         rows = 2
-        (f, axes) = plt.subplots(rows, 1, sharex=True)
+        (fig, axes) = plt.subplots(rows, 1, sharex=True)
         (ax_elevation, ax_cadence) = axes
     # not speed, not hr, not cad
     # 0, 0, 0
     else:
         rows = 1
-        (f, axes) = plt.subplots(rows, 1, sharex=True)
+        (fig, axes) = plt.subplots(rows, 1, sharex=True)
         ax_elevation = axes
-    return (f, (ax_elevation, ax_speed, ax_hr, ax_cadence))
+    return (fig, (ax_elevation, ax_speed, ax_hr, ax_cadence))
 
 
 def main():
@@ -222,12 +219,12 @@ def main():
     markers = np.append(markers, len(elevations) - 1)
 
     # km
-    x = np.cumsum(distances)/1000
+    cumulative_distances = np.cumsum(distances)/1000
 
     if not args.quiet:
         print("Calculating metrics")
 
-    (grades, speed_averages, heart_rate_averages, cadence_percentages) = calculate_metrics(markers, distances, x, times, elevations, heart_rates, cadences)
+    (grades, speed_averages, heart_rate_averages, cadence_percentages) = calculate_metrics(markers, distances, cumulative_distances, times, elevations, heart_rates, cadences)
 
     if not args.quiet:
         print("Plotting")
@@ -246,33 +243,33 @@ def main():
     else:
         plt.ioff()
 
-    (f, (ax_elevation, ax_speed, ax_hr, ax_cadence)) = get_figure(args, heart_rates, cadences)
+    (fig, (ax_elevation, ax_speed, ax_hr, ax_cadence)) = get_figure(args, heart_rates, cadences)
     axes = tuple([a for a in (ax_elevation, ax_speed, ax_hr, ax_cadence) if a])
 
     # ax_elevation.plot(x, elevations, color=green, label="Raw Elevation", fillstyle="bottom")
     # TODO: gradients
-    ax_elevation.fill_between(x, elevations, 0, color=green, alpha=0.5)
+    ax_elevation.fill_between(cumulative_distances, elevations, 0, color=green, alpha=0.5)
 
     # calculate the smoothed gradients and create a colour map for it
-    gg = np.abs(gradient)
+    gradient_abs = np.abs(gradient)
     # first stretch the data to be in the range [0,1]
-    gg = gg/gg.max()
-    gg = np.clip(gg, 0, _GRADIENT_CLIPPING_FACTOR)/_GRADIENT_CLIPPING_FACTOR
+    gradient_normalised = gradient_abs/gradient_abs.max()
+    gradient_clipped = np.clip(gradient_normalised, 0, _GRADIENT_CLIPPING_FACTOR)/_GRADIENT_CLIPPING_FACTOR
     cmap = colors.ListedColormap(sns.color_palette([yellow, orange, red]).as_hex())
-    sc = ax_elevation.scatter(x[:-1], elevations_filtered[:-1], c=cmap(gg), s=0.1, edgecolor=None)
+    ax_elevation.scatter(cumulative_distances[:-1], elevations_filtered[:-1], c=cmap(gradient_clipped), s=0.1, edgecolor=None)
 
     # plot the smoothed elevations, coloured according to the smoothed gradient
     # https://matplotlib.org/3.1.0/gallery/lines_bars_and_markers/multicolored_line.html
-    elevation_points = np.array([x, elevations_filtered]).T.reshape(-1, 1, 2)
+    elevation_points = np.array([cumulative_distances, elevations_filtered]).T.reshape(-1, 1, 2)
     elevation_segments = np.concatenate([elevation_points[:-1], elevation_points[1:]], axis=1)
     elevation_lines = collections.LineCollection(elevation_segments, cmap=cmap)
-    elevation_lines.set_array(gg)
+    elevation_lines.set_array(gradient_clipped)
     ax_elevation.add_collection(elevation_lines)
     # line = ax_e...
     # TODO: f.colorbar(line, ax=ax_elevation)
 
     # other plot stuffs
-    ax_elevation.set_xlim(min(x), max(x))
+    ax_elevation.set_xlim(min(cumulative_distances), max(cumulative_distances))
     ax_elevation.set_xlabel("Distance (km)", fontsize=_FONT_SIZE)
     ax_elevation.set_ylim(0, max(elevations)*(1 + _PLOT_PADDING))
     ax_elevation.set_ylabel("m", fontsize=_FONT_SIZE)
@@ -280,20 +277,20 @@ def main():
     ax_elevation.grid()
 
     ax_grade = ax_elevation.twinx()
-    ax_grade.set_xlim(min(x), max(x))
+    ax_grade.set_xlim(min(cumulative_distances), max(cumulative_distances))
     grades_max = np.ceil(max([abs(g) for g in grades]))
     # make symmetric
     ax_grade.set_ylim(-grades_max*(1 + _PLOT_PADDING), grades_max*(1 + _PLOT_PADDING))
-    ax_grade.plot(x[:-1], np.array(grades), color=orange, alpha=0.7, label="Stepped Grade")
+    ax_grade.plot(cumulative_distances[:-1], np.array(grades), color=orange, alpha=0.7, label="Stepped Grade")
     ax_grade.set_ylabel("%", fontsize=_FONT_SIZE)
     ax_grade.tick_params(labelsize=_FONT_SIZE)
     ax_grade.grid()
 
     if ax_speed:
-        ax_speed.set_xlim(min(x), max(x))
+        ax_speed.set_xlim(min(cumulative_distances), max(cumulative_distances))
         ax_speed.set_ylim(min(speed_averages)*(1 - _PLOT_PADDING), max(speed_averages)*(1 + _PLOT_PADDING))
-        ax_speed.plot(x[:-1], np.array(speed_averages), color=cyan, label="Average Speed")
-        ax_speed.plot(x[:-1], speed[:-1], color=cyan, alpha=0.3, label="Speed")
+        ax_speed.plot(cumulative_distances[:-1], np.array(speed_averages), color=cyan, label="Average Speed")
+        ax_speed.plot(cumulative_distances[:-1], speed[:-1], color=cyan, alpha=0.3, label="Speed")
         ax_speed.set_xlabel("Distance (km)", fontsize=_FONT_SIZE)
         ax_speed.set_ylabel("km/h", fontsize=_FONT_SIZE)
         ax_speed.set_ylim(0, max(speed)*(1 + _PLOT_PADDING))
@@ -302,10 +299,10 @@ def main():
         ax_speed.grid()
 
     if ax_hr:
-        ax_hr.set_xlim(min(x), max(x))
+        ax_hr.set_xlim(min(cumulative_distances), max(cumulative_distances))
         ax_hr.set_ylim(min(heart_rate_averages)*(1 - _PLOT_PADDING), max(heart_rate_averages)*(1 + _PLOT_PADDING))
-        ax_hr.plot(x[:-1], np.array(heart_rate_averages), color=red, label="Average Heart Rate")
-        ax_hr.plot(x[:-1], heart_rates[:-2], color=red, alpha=0.3, label="Heart Rate")
+        ax_hr.plot(cumulative_distances[:-1], np.array(heart_rate_averages), color=red, label="Average Heart Rate")
+        ax_hr.plot(cumulative_distances[:-1], heart_rates[:-2], color=red, alpha=0.3, label="Heart Rate")
         ax_hr.set_xlabel("Distance (km)", fontsize=_FONT_SIZE)
         ax_hr.set_ylabel("BPM", fontsize=_FONT_SIZE)
         ax_hr.legend(loc="upper right", fontsize=_FONT_SIZE)
@@ -313,11 +310,11 @@ def main():
         ax_hr.grid()
 
     if ax_cadence:
-        ax_cadence.set_xlim(min(x), max(x))
+        ax_cadence.set_xlim(min(cumulative_distances), max(cumulative_distances))
         # ax_cadence.set_ylim(min(cadence_averages)*(1 - _PLOT_PADDING), max(cadence_averages)*(1 + _PLOT_PADDING))
-        # ax_cadence.plot(x[:-1], np.array(cadence_averages), color=purple, label="Average Cadence")
-        ax_cadence.plot(x[:-1], np.array(cadence_percentages)*100, color=purple, label="Percentage Pedaling")
-        ax_cadence.plot(x[:-1], cadences[:-2], color=purple, alpha=0.3, label="Cadence")
+        # ax_cadence.plot(cumulative_distances[:-1], np.array(cadence_averages), color=purple, label="Average Cadence")
+        ax_cadence.plot(cumulative_distances[:-1], np.array(cadence_percentages)*100, color=purple, label="Percentage Pedaling")
+        ax_cadence.plot(cumulative_distances[:-1], cadences[:-2], color=purple, alpha=0.3, label="Cadence")
         ax_cadence.set_xlabel("Distance (km)", fontsize=_FONT_SIZE)
         ax_cadence.set_ylabel("RPM / %", fontsize=_FONT_SIZE)
         ax_cadence.set_ylim(0, max(cadences)*(1 + _PLOT_PADDING))
@@ -326,15 +323,15 @@ def main():
         ax_cadence.grid()
 
     # h1, l1 = ax_elevation.get_legend_handles_labels()
-    h2, l2 = ax_grade.get_legend_handles_labels()
+    handles_ax_grade, legend_ax_grade = ax_grade.get_legend_handles_labels()
     # https://stackoverflow.com/questions/4700614/how-to-put-the-legend-out-of-the-plot
-    ax_grade.legend(h2, l2, loc="upper right", fontsize=_FONT_SIZE)
+    ax_grade.legend(handles_ax_grade, legend_ax_grade, loc="upper right", fontsize=_FONT_SIZE)
 
-    f.align_ylabels(axes)
+    fig.align_ylabels(axes)
 
     if track.name:
         # TODO: time
-        f.suptitle("{} on {}".format(track.name.strip(), track.get_time_bounds().start_time.date()))
+        fig.suptitle("{} on {}".format(track.name.strip(), track.get_time_bounds().start_time.date()))
 
     (input_basename, _) = os.path.splitext(os.path.basename(args.filename))
     output_filename = ".".join([input_basename, "png"])
